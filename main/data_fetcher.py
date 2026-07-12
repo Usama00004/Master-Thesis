088af3a628d939bfd54c518b459bad4e679d3ba5
Hibra Tahir <usamatahir00004@gmail.com>
2026-07-12 13:23:18 +0200

    fetching data

--- main/data_fetcher.py
+++ main/data_fetcher.py
@@ -21,34 +21,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import kagglehub
-# from kagglehub import KaggleDatasetAdapter
-
-# file_path = "UCI_Credit_Card.csv"
-
-# df = kagglehub.load_dataset(
-#     KaggleDatasetAdapter.PANDAS,
-#     "uciml/default-of-credit-card-clients-dataset",
-#     file_path,
-# )
-
-# print("First 5 records:")
-# print(df.head())
