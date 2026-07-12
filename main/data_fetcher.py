3d89c5798aa527eb639f18fac205b7559f3cb9b4
Hibra Tahir <usamatahir00004@gmail.com>
2026-07-12 13:42:34 +0200

    changing column names

--- main/data_fetcher.py
+++ main/data_fetcher.py
@@ -1,6 +1,7 @@
 import kagglehub
 from kagglehub import KaggleDatasetAdapter
 
+
 def load_credit_card_dataset():
     file_path = "UCI_Credit_Card.csv"
     df = kagglehub.load_dataset(
@@ -10,13 +11,22 @@ def load_credit_card_dataset():
     )
     return df
 
-def display_dataset_info(df):
-    print("First 5 records:")
-    print(df.head())
 
+def display_dataset_info(df):
+    print("-----------Orignal Column Names---------")
+    print(df.columns.tolist())
+    return df
+
+
+def data_columns_changer(df):
+    df.columns = [f"col{i}" for i in range(1, len(df.columns) + 1)]
+    print("-----------Dummy Column Names---------")
+    print(df.columns.tolist())
+    
 def main():
     df = load_credit_card_dataset()
     display_dataset_info(df)
+    data_columns_changer(df)
 
 if __name__ == "__main__":
     main()
